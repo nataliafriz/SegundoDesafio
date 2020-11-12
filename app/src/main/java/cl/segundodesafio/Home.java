bcb5e6d7c729134c4aabe34dfaca42c494c07f6b
Natalia Friz <nataliafriz.3@gmail.com>
2020-11-12 14:45:56 -0300

    mensaje2

---
+++ app/src/main/java/cl/segundodesafio/Home.java
@@ -0,0 +1,25 @@
+package cl.segundodesafio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayoutStates;
+
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+
+
+    @Override
+    protected final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
+
+
+    
+    
+
+
